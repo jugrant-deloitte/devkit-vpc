73c792a3de9a2c8df445869ad5ca7125b3e41444
Justin Grant <jugrant@deloitte.com>
2021-03-25 15:04:59 -0400

    missed saves

--- devkit-destroy-vpc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-clear
-sudo podman run -it --rm \
-    --name devkit-vpc \
-    --entrypoint ./breakdown.yml \
-    --workdir /root/deploy/terraform/devkit-vpc \
-    --volume $(pwd):/root/deploy/terraform/devkit-vpc:z \
-    --volume $(pwd)/aws:/root/.aws:z \
-  docker.io/cloudctl/konductor
